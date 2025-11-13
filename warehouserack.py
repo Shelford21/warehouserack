d4aaa15917c572f5060ae5d040a8ec1b2be62624
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-11-13 15:21:33 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- warehouserack.py
+++ warehouserack.py
@@ -97,7 +97,7 @@ if st.button("🔄 Refresh Data"):
 
 st.header("✏️ Edit Berdasarkan PO, Kode, dan Material")
 
-name_list = name.iloc[0:30000, 8].dropna().astype(str).unique().tolist()
+name_list = name.iloc[0:30000, 1].dropna().astype(str).unique().tolist()
 name_list.insert(0, "-")
 selected_name = st.selectbox("Pilih PO:", name_list)
 
